863c899fdb7003eb72a51bc46640c1d9f6603bae
Marlon O'Neil <hello@marlononeil.com>
2017-08-30 14:05:47 -0400

    added links to Welcome and About page using route helpers

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get '/welcome'      => 'pages#welcome'
+  root 'pages#welcome'
   get '/'             => 'pages#welcome'
   get '/about'        => 'pages#about'
   get '/contest'      => 'pages#contest'
-  get '/kitten/:size' => 'pages#kitten'
+  get '/kitten/:size' => 'pages#kitten', as: 'kitten'
   get '/kittens/:size' => 'pages#kittens'
   get '/secrets/:magic_word' => 'pages#secrets'
 
